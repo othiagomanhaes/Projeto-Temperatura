93f90691b40ee7c6dbe1cb0427611d930440d370
Thiago Manhães <85084455+othiagomanhaes@users.noreply.github.com>
2022-04-19 00:02:00 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -4,7 +4,7 @@ import json
 
 lat = float(input('Diga-me sua Latitude: '))
 lon = float(input('Diga-me sua Longittude: '))
-APIkey = '0171b43cbe89768af1f26188e66bea16'
+APIkey = ''
 temperatura = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}')
 temperatura = temperatura.json()
 print(f"{temperatura['name']} = {(temperatura['main']['temp'] - 273.15):.2f}ºC")
